1f1e43bbe1ad8253a14ee9ed9ac125974b18b275
Naldo Wang <1165648703@qq.com>
2019-11-07 18:42:21 +0800

    配置eslint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    "browser": true,
+    "node": true,
+    "es6": true
+  },
+  extends: [
+    "plugin:vue/essential"
+  ],
+  globals: {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  parserOptions: {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  plugins: [
+    "vue"
+  ],
+  rules: {
+  }
+}

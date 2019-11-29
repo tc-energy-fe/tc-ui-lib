434ad1743d053d5d5e4517308c1c86bfa7d2af72
Naldo Wang <1165648703@qq.com>
2019-11-29 18:01:35 +0800

    Input组件

--- example/App.vue
+++ example/App.vue
@@ -2,16 +2,25 @@
   <div id="app">
     <h2>基础组件</h2>
     <h4>Input</h4>
-    <eg-input></eg-input>
+    <eg-input v-model="inputText">
+      <template v-slot:suffix>
+        kWh/天
+      </template>
+    </eg-input>
     <h4>Box</h4>
     <eg-box></eg-box>
-    <i class="icon-username">&#xe66a;</i>
+    <i class="iconfont icon-yonghuming"></i>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'App'
+    name: 'App',
+    data () {
+      return {
+        inputText: ''
+      }
+    }
   }
 </script>
 
@@ -20,24 +29,27 @@
     padding: 0;
     margin: 0;
   }
-  @font-face {
-    font-family: 'iconfont';  /* project id 444910 */
-    src: url('//at.alicdn.com/t/font_444910_ucrq3qghqi.eot');
-    src: url('//at.alicdn.com/t/font_444910_ucrq3qghqi.eot?#iefix') format('embedded-opentype'),
-    url('//at.alicdn.com/t/font_444910_ucrq3qghqi.woff2') format('woff2'),
-    url('//at.alicdn.com/t/font_444910_ucrq3qghqi.woff') format('woff'),
-    url('//at.alicdn.com/t/font_444910_ucrq3qghqi.ttf') format('truetype'),
-    url('//at.alicdn.com/t/font_444910_ucrq3qghqi.svg#iconfont') format('svg');
-  }
+
+  @import "http://at.alicdn.com/t/font_444910_ucrq3qghqi.css";
+  /*@font-face {*/
+  /*  font-family: 'iconfont';  !* project id 444910 *!*/
+  /*  src: url('//at.alicdn.com/t/font_444910_ucrq3qghqi.eot');*/
+  /*  src: url('//at.alicdn.com/t/font_444910_ucrq3qghqi.eot?#iefix') format('embedded-opentype'),*/
+  /*  url('//at.alicdn.com/t/font_444910_ucrq3qghqi.woff2') format('woff2'),*/
+  /*  url('//at.alicdn.com/t/font_444910_ucrq3qghqi.woff') format('woff'),*/
+  /*  url('//at.alicdn.com/t/font_444910_ucrq3qghqi.ttf') format('truetype'),*/
+  /*  url('//at.alicdn.com/t/font_444910_ucrq3qghqi.svg#iconfont') format('svg');*/
+  /*}*/
 
   #app{
     background-color: #e5e5e5;
     padding: 1rem;
     font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
   }
-
-  .icon-username {
-    font-family: iconfont !important;
-    display: inline-block;
-  }
+  /*.iconfont {*/
+  /*  font-family: iconfont !important;*/
+  /*  font-style: normal;*/
+  /*  font-size: 2rem;*/
+  /*  display: inline-block;*/
+  /*}*/
 </style>
--- src/components/input/src/input.vue
+++ src/components/input/src/input.vue
@@ -1,12 +1,45 @@
 <template>
   <div class="eg-input">
-    <input>
+    <input
+      class="eg-input__inner"
+      :value="value"
+      v-bind="$attrs"
+      :type="type"
+      :disabled="disabled"
+      :readonly="readonly"
+      @input="handleInput"
+    >
+    <span
+      v-if="$slots.suffix"
+      class="eg-input__suffix"
+    >
+      <slot name="suffix"></slot>
+    </span>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'EgInput'
+    name: 'EgInput',
+    inheritAttrs: false,
+    props: {
+      value: [String, Number],
+      type: {
+        type: String,
+        default: 'text'
+      },
+      disabled: Boolean,
+      readonly: Boolean,
+    },
+    data () {
+      return {
+      }
+    },
+    methods: {
+      handleInput (event) {
+        this.$emit('input', event.target.value)
+      }
+    }
   }
 </script>
 
